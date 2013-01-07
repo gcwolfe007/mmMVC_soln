0a6edd37e944ba59f8fd6115a94ea58d84238ebc
Clifford Wolfe <gcwolfe007@gmail.com>
2013-01-06 19:26:17 -0500

    1930 hrs

--- mmMVC/Controllers/HomeController.cs
+++ mmMVC/Controllers/HomeController.cs
@@ -12,10 +12,10 @@ namespace mmMVC.Controllers
         public ActionResult Index()
         {
 
-     
 
 
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            ViewBag.Message = "Don't Overpay. Book Online & Save! ";
 
             return View();
         }
